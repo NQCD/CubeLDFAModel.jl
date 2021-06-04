e7ec8c32584bb1a1b2f91d456c181d9670fdd2d4
James Gardner <james.gardner1421@gmail.com>
2021-06-04 22:54:24 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/CubeLDFAModel.jl
@@ -0,0 +1,5 @@
+module CubeLDFAModel
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using CubeLDFAModel
+using Test
+
+@testset "CubeLDFAModel.jl" begin
+    # Write your tests here.
+end
