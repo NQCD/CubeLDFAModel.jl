f0e07b66c9ffe2bb71ade836c2c01a1850daebfe
James Gardner <james.gardner1421@gmail.com>
2021-08-22 13:17:54 +0100

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,38 @@
 using NonadiabaticModels: Free
-using NonadiabaticDynamicsBase: PeriodicCell, Atoms
+using PyCall
+using NonadiabaticDynamicsBase
 using CubeLDFAModel
 using Test
-
-@testset "CubeLDFAModel.jl" begin
-
-    vecs = [0.111175    0.000000    0.000000
-           -0.055588    0.096280    0.000000
-            0.000000    0.000000    0.703079]' .* 100
-    model = LDFAModel(Free(), "test.cube", Atoms([:H]), PeriodicCell(vecs))
-end
+using Unitful
 
 include("test_cube.jl")
+
+aseio = pyimport("ase.io")
+ase_atoms = aseio.read("start.in")
+atoms, R, cell =  NonadiabaticDynamicsBase.convert_from_ase_atoms(ase_atoms)
+
+cube_offset = [0.027794, 0.048140, 0.351539]
+model = LDFAModel(Free(), "test.cube", atoms, cell; friction_atoms=[1, 2], cube_offset=cube_offset)
+
+@testset "friction!" begin
+
+    F = zeros(6, 6)
+    r = @view R[:,1]
+    r .= 0
+    CubeLDFAModel.friction!(model, F, R)
+
+    for _=1:10
+        r .= 0
+        r += rand() * cell.vectors[:,1]
+        r += rand() * cell.vectors[:,2]
+        r += rand() * cell.vectors[:,3]
+
+        CubeLDFAModel.friction!(model, F, R)
+    end
+
+    r = cell.vectors[:,1] + cell.vectors[:,2] + cell.vectors[:,3]
+    CubeLDFAModel.friction!(model, F, R)
+
+    r = cell.vectors[:,1] + cell.vectors[:,2] + cell.vectors[:,3] + rand(3)
+    CubeLDFAModel.friction!(model, F, R)
+end
